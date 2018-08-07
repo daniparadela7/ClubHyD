1cfdac7cc1cfd79ccf93a277c1b0510752e84fe4
daniparadela7 <danipar10@hotmail.com>
2018-08-07 10:30:29 +0200

    Update homeConfig.js

--- app/sections/duendes/home/homeConfig.js
+++ app/sections/duendes/home/homeConfig.js
@@ -6,9 +6,9 @@ angular.module('app')
 		controller: 'HomeController',
 		templateUrl: 'sections/duendes/home/homeView.html',
 		meta: {
-	    	'title': 'El Club de los Duendes. Cumpleaños, talleres, bautizos y comuniones',
-	    	'description': 'El Club de los Duendes de Palma de Mallorca realiza fiestas de cumpleaños, talleres para padres y niños, bautizos, comuniones y charlas.'
-	    },
+      'title': 'El Club de los Duendes. Cumpleaños, talleres, bautizos y comuniones',
+      'description': 'El Club de los Duendes de Palma de Mallorca realiza fiestas de cumpleaños, talleres para padres y niños, bautizos, comuniones y charlas.'
+    },
 		resolve: {
 			delayForTransitions: function($timeout) {
                 return $timeout(function(){
