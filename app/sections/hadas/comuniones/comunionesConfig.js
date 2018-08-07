ffc232c2f8fbee1ebc25e9833f417ff48574057a
daniparadela7 <danipar10@hotmail.com>
2018-08-07 10:36:03 +0200

    Update comunionesConfig.js

--- app/sections/hadas/comuniones/comunionesConfig.js
+++ app/sections/hadas/comuniones/comunionesConfig.js
@@ -6,9 +6,9 @@ angular.module('app')
 		controller: 'ComunionesController',
 		templateUrl: 'sections/hadas/comuniones/comunionesView.html',
 		meta: {
-	    	'title': 'Comuniones | El Club de las Hadas',
-	    	'description': 'Aquí encontrarás toda las información relacionada con las comuniones que realizamos en El Club de las Hadas de Palma de Mallorca.'
-	    },
+      'title': 'Comuniones | El Club de las Hadas',
+      'description': 'Aquí encontrarás toda las información relacionada con las comuniones que realizamos en El Club de las Hadas de Palma de Mallorca.'
+    },
 		resolve: {
 			delayForTransitions: function($timeout) {
                 return $timeout(function(){
@@ -21,9 +21,9 @@ angular.module('app')
 		controller: 'ComunionesController',
 		templateUrl: 'sections/hadas/comuniones/comunionesView.html',
 		meta: {
-	    	'title': 'Comuniones | El Club de los Duendes',
-	    	'description': 'Aquí encontrarás toda las información relacionada con las comuniones que realizamos en El Club de los Duendes de Palma de Mallorca.'
-	    },
+      'title': 'Comuniones | El Club de los Duendes',
+      'description': 'Aquí encontrarás toda las información relacionada con las comuniones que realizamos en El Club de los Duendes de Palma de Mallorca.'
+    },
 		resolve: {
 			delayForTransitions: function($timeout) {
                 return $timeout(function(){
