fa384fd67215b9a2acef50e6bad9bfe8ee8adfc5
daniparadela7 <danipar10@hotmail.com>
2018-08-07 10:34:25 +0200

    Update cafeteriaConfig.js

--- app/sections/hadas/cafeteria/cafeteriaConfig.js
+++ app/sections/hadas/cafeteria/cafeteriaConfig.js
@@ -6,12 +6,12 @@ angular.module('app')
 		controller: 'CafeteriaController',
 		templateUrl: 'sections/hadas/cafeteria/cafeteriaView.html',
 		meta: {
-	    	'title': 'Cafeteria | El Club de las Hadas',
-	    	'description': 'Aquí encontrarás toda las información relacionada con El Club de las Hadas de Palma de Mallorca. Horarios, cartas...'
-	    },
+      'title': 'Cafeteria | El Club de las Hadas',
+      'description': 'Aquí encontrarás toda las información relacionada con El Club de las Hadas de Palma de Mallorca. Horarios, cartas...'
+    },
 		resolve: {
 			cafeteria: function(apiService) {
-			    return apiService.getNodes('cafeteria');
+        return apiService.getNodes('cafeteria');
 			},
 			delayForTransitions: function($timeout) {
                 return $timeout(function(){
@@ -24,12 +24,12 @@ angular.module('app')
 		controller: 'CafeteriaController',
 		templateUrl: 'sections/hadas/cafeteria/cafeteriaView.html',
 		meta: {
-	    	'title': 'Cafeteria | El Club de los Duendes',
-	    	'description': 'Aquí encontrarás toda las información relacionada con El Club de los Duendes de Palma de Mallorca. Horarios, cartas...'
-	    },
+      'title': 'Cafeteria | El Club de los Duendes',
+      'description': 'Aquí encontrarás toda las información relacionada con El Club de los Duendes de Palma de Mallorca. Horarios, cartas...'
+    },
 		resolve: {
 			cafeteria: function(apiService) {
-			    return apiService.getNodes('cafeteria');
+        return apiService.getNodes('cafeteria');
 			},
 			delayForTransitions: function($timeout) {
                 return $timeout(function(){
