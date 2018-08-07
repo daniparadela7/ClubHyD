897e8b41794ccbf0066d92dbfc7941191361158b
daniparadela7 <danipar10@hotmail.com>
2018-08-07 10:36:33 +0200

    Update cumpleañosConfig.js

--- app/sections/hadas/cumpleaños/cumpleañosConfig.js
+++ app/sections/hadas/cumpleaños/cumpleañosConfig.js
@@ -6,9 +6,9 @@ angular.module('app')
 		controller: 'CumpleañosController',
 		templateUrl: 'sections/hadas/cumpleaños/cumpleañosView.html',
 		meta: {
-	    	'title': 'Cumpleaños | El Club de las Hadas',
-	    	'description': 'Aquí encontrarás toda las información relacionada con los cumpleaños que realizamos en El Club de las Hadas de Palma de Mallorca.'
-	    },
+      'title': 'Cumpleaños | El Club de las Hadas',
+      'description': 'Aquí encontrarás toda las información relacionada con los cumpleaños que realizamos en El Club de las Hadas de Palma de Mallorca.'
+    },
 		resolve: {
 			delayForTransitions: function($timeout) {
                 return $timeout(function(){
