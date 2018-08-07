a3a81a0bd7dc136df085fa6abc96c86a7b699b07
daniparadela7 <danipar10@hotmail.com>
2018-08-07 10:50:32 +0200

    Update noticiaController.js

--- app/sections/hadas/noticias/noticia/noticiaController.js
+++ app/sections/hadas/noticias/noticia/noticiaController.js
@@ -3,5 +3,5 @@ angular.module('app')
 .controller('NoticiaController', ['$scope', 'noticia', function($scope, noticia){
 	$scope.noticia = noticia;
 	$scope.fecha_noticiaFormato = moment(noticia.fecha_noticia).format('DD/MM/YYYY HH:mm');
-	
+
 }]);
