88073bb996a764174f1fc71683c41804b0a30f07
daniparadela7 <danipar10@hotmail.com>
2018-08-07 10:31:23 +0200

    Update parqueConfig.js

--- app/sections/duendes/parque/parqueConfig.js
+++ app/sections/duendes/parque/parqueConfig.js
@@ -6,9 +6,9 @@ angular.module('app')
 		controller: 'ParqueController',
 		templateUrl: 'sections/duendes/parque/parqueView.html',
 		meta: {
-	    	'title': 'Parque de bolas | El Club de los Duendes',
-	    	'description': 'Aquí encontrarás toda la información relacionada con el parque de bolas de El Club de los Duendes de Palma de Mallorca.'
-	    },
+      'title': 'Parque de bolas | El Club de los Duendes',
+      'description': 'Aquí encontrarás toda la información relacionada con el parque de bolas de El Club de los Duendes de Palma de Mallorca.'
+    },
 		resolve: {
 			delayForTransitions: function($timeout) {
                 return $timeout(function(){
