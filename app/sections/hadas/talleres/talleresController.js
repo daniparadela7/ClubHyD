94df5a7ffc7fd87b3c36cccd4feede8a65cbf72a
daniparadela7 <danipar10@hotmail.com>
2018-08-07 10:50:05 +0200

    Update talleresController.js

--- app/sections/hadas/talleres/talleresController.js
+++ app/sections/hadas/talleres/talleresController.js
@@ -2,20 +2,20 @@
 angular.module('app')
 .controller('TalleresController', ['$scope', '$timeout', function($scope, $timeout){
 
-	$scope.img_manualidades = ["../../../assets/images/hadas/talleres/manualidades1.jpg",
-								"../../../assets/images/hadas/talleres/manualidades2.jpg"];
+	$scope.img_manualidades = ['../../../assets/images/hadas/talleres/manualidades1.jpg',
+								'../../../assets/images/hadas/talleres/manualidades2.jpg'];
 
-	$scope.img_costura = ["../../../assets/images/hadas/talleres/costura1.jpg",
-								"../../../assets/images/hadas/talleres/costura2.jpg"];
+	$scope.img_costura = ['../../../assets/images/hadas/talleres/costura1.jpg',
+								'../../../assets/images/hadas/talleres/costura2.jpg'];
 
-	$scope.img_reposteria = ["../../../assets/images/hadas/talleres/reposteria1.jpg",
-								"../../../assets/images/hadas/talleres/reposteria2.jpg"];
+	$scope.img_reposteria = ['../../../assets/images/hadas/talleres/reposteria1.jpg',
+								'../../../assets/images/hadas/talleres/reposteria2.jpg'];
 
-	$scope.img_belleza = ["../../../assets/images/hadas/talleres/belleza1.jpg",
-								"../../../assets/images/hadas/talleres/belleza2.jpg"];
+	$scope.img_belleza = ['../../../assets/images/hadas/talleres/belleza1.jpg',
+								'../../../assets/images/hadas/talleres/belleza2.jpg'];
 
-	$scope.img_musica = ["../../../assets/images/hadas/talleres/musical1.jpg",
-								"../../../assets/images/hadas/talleres/musical2.jpg"];																													
+	$scope.img_musica = ['../../../assets/images/hadas/talleres/musical1.jpg',
+								'../../../assets/images/hadas/talleres/musical2.jpg'];
 
 	$timeout(function(){
 
@@ -24,9 +24,9 @@ angular.module('app')
 			smartSpeed: 450,
 			nav: true,
 			navText: [
-						      "<i class='fa fa-chevron-circle-left'></i>",
-						      "<i class='fa fa-chevron-circle-right'></i>"
-						    ],
+                  '<i class='fa fa-chevron-circle-left'></i>',
+                  '<i class='fa fa-chevron-circle-right'></i>'
+                ],
 			dots: false,
 			loop: true,
 			autoplay: false,
@@ -35,6 +35,6 @@ angular.module('app')
 			responsiveClass: true,
 			items: 1
 		});
-		
+
 	}, 500);
 }]);
