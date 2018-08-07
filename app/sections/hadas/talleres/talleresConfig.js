9099ead8c0bb58e7453f6e944a2bc465c041dfaf
daniparadela7 <danipar10@hotmail.com>
2018-08-07 10:47:42 +0200

    Update talleresConfig.js

--- app/sections/hadas/talleres/talleresConfig.js
+++ app/sections/hadas/talleres/talleresConfig.js
@@ -6,9 +6,9 @@ angular.module('app')
 		controller: 'TalleresController',
 		templateUrl: 'sections/hadas/talleres/talleresView.html',
 		meta: {
-	    	'title': 'Talleres | El Club de las Hadas',
-	    	'description': 'Aquí encontrarás toda la información relacionada con los talleres de El Club de las Hadas de Palma de Mallorca.'
-	    },
+    'title': 'Talleres | El Club de las Hadas',
+    'description': 'Aquí encontrarás toda la información relacionada con los talleres de El Club de las Hadas de Palma de Mallorca.'
+    },
 		resolve: {
 			delayForTransitions: function($timeout) {
                 return $timeout(function(){
@@ -21,9 +21,9 @@ angular.module('app')
 		controller: 'TalleresController',
 		templateUrl: 'sections/hadas/talleres/talleresView.html',
 		meta: {
-	    	'title': 'Talleres | El Club de los Duendes',
-	    	'description': 'Aquí encontrarás toda la información relacionada con los talleres de El Club de los Duendes de Palma de Mallorca.'
-	    },
+      'title': 'Talleres | El Club de los Duendes',
+      'description': 'Aquí encontrarás toda la información relacionada con los talleres de El Club de los Duendes de Palma de Mallorca.'
+    },
 		resolve: {
 			delayForTransitions: function($timeout) {
                 return $timeout(function(){
