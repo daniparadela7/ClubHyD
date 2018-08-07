b01766336ed925e9c3fc1b6b3cf0f9a81e8ad679
daniparadela7 <danipar10@hotmail.com>
2018-08-07 10:46:03 +0200

    Update noticiasController.js

--- app/sections/hadas/noticias/noticiasController.js
+++ app/sections/hadas/noticias/noticiasController.js
@@ -2,9 +2,9 @@
 angular.module('app')
 .controller('NoticiasController', ['$scope', 'noticias', function($scope, noticias){
 	$scope.noticias = [];
-	angular.forEach(noticias, function(value, key) {
+	angular.forEach(noticias, function(value) {
 		value.fecha_noticiaFormato = moment(value.fecha_noticia).format('DD/MM/YY');
 		$scope.noticias.push(value);
 	});
-	
+
 }]);
